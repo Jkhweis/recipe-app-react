1031b7d4163faed66452f112e1475c0b26570711
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-04-06 17:41:30 -0500

    Fetching a Firestore Collection

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,41 @@
-import { useFetch } from '../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
+import { useState, useEffect } from 'react';
+
+//comps
 import RecipeList from '../../comps/RecipeList';
 
 //styles
 import './Home.css';
 
 export default function Home() {
-  const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore
+      .collection('recipes')
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError('No recipies to load');
+          setIsPending(false);
+        } else {
+          let results = [];
+          snapshot.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() });
+          });
+          setData(results);
+          setIsPending(false);
+        }
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
